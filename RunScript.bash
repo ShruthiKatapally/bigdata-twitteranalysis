9658296ae0c2971887ecf34395220a89fa31eb50
Rakesh Menon <rcmenon@indiana.edu>
2014-12-18 16:52:57 -0500

    adding bash scripts for automated mongo install

--- RunScript.bash
+++ RunScript.bash
@@ -1,8 +1,14 @@
+apt-get install default-jre
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
 apt-get install -y mongodb-org
 
 service mongod start
+
+
+cd /home/ubuntu/twitter-analysis/
+
+java -jar DataModels-Mongo.jar
 
 mongo
